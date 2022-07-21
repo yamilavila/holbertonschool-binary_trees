4102fa06e0860f9eb6b102ace4d7fc1cd94baa0d
yamilavila <git@github.com>
2022-07-21 02:43:59 +0000

    task 1, done betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a new node as the left-child of another node
+ * binary_tree_insert_left - inserts a new node as the left-child
  * @parent: pointer to the new node to insert the left-child in
  * @value: value to store in the new node
  * Return: pointer to the new node
@@ -10,11 +10,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node;
 
-	if(!parent)
+	if (!parent)
 		return (NULL);
 	new_left_node = binary_tree_node(parent, value);
 	if (!new_left_node)
-		return(NULL);
+		return (NULL);
 	new_left_node->left = parent->left;
 	if (parent->left)
 		parent = new_left_node;
