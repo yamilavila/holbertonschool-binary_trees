8b4343b279f2bc11a47de89ef12521b7fc2eb4eb
yamilavila <git@github.com>
2022-07-21 21:40:14 +0000

    Task 10 debuged

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,11 +9,12 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
-	if (tree == NULL || tree->parent != NULL)
+	if (tree == NULL)
 		return (0);
+	if (tree->parent != NULL)
 	{
-	depth = 1 + binary_tree_depth(tree->parent);
-	return (depth);
+		depth = 1 + binary_tree_depth(tree->parent);
+		return (depth);
 	}
 	return (0);
 }
