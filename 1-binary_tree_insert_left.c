3b8852c30ba8576a17afeddd94acaadb8e244ece
yamilavila <git@github.com>
2022-07-21 12:19:21 +0000

    task 1, left node fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,13 +12,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	new_left_node = binary_tree_node(parent, value);
+
+	new_left_node = malloc(sizeof(binary_tree_t));
 	if (new_left_node == NULL)
 		return (NULL);
-	if (parent->left != NULL)
-		new_left_node->left = parent->left;
-	parent->left->parent = new_left_node;
-	parent->left = new_left_node;
-	
+	new_left_node->parent = parent;
+	new_left_node->n = value;
+	new_left_node->right = NULL;
+	new_left_node->left = parent->left;
+	if (new_left_node->left != NULL)
+		new_left_node->left->parent = new_left_node;
+	parent->left = new_left_node;	
 	return (new_left_node);
 }
