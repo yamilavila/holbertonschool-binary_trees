dd6b9ada6544cc5098fff1d5051c5278f299e1e2
yamilavila <git@github.com>
2022-07-21 21:23:36 +0000

    typo fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,7 +12,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL || tree->parent != NULL)
 		return (0);
 	{
-	depth = 1 + binary_tree_dept(tree->parent);
+	depth = 1 + binary_tree_depth(tree->parent);
 	return (depth);
 	}
 	return (0);
