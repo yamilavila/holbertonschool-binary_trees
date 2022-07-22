bb6bfda134701eaf8fc58022d44d480a00189f65
yamilavila <git@github.com>
2022-07-22 00:07:58 +0000

    Task 16 & 17

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,18 +8,18 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t size_tree = 1;
+	size_t size_tree = 1;
 
-        if (tree == NULL)
-                return (0);
-        if (tree != NULL)
-        {
-                if (tree->left)
-                        size_tree += binary_tree_size(tree->left);
-                if (tree->right)
-                        size_tree += binary_tree_size(tree->right);
-        }
-        return (size_tree);
+	if (tree == NULL)
+		return (0);
+	if (tree != NULL)
+	{
+		if (tree->left)
+			size_tree += binary_tree_size(tree->left);
+		if (tree->right)
+			size_tree += binary_tree_size(tree->right);
+	}
+	return (size_tree);
 }
 
 /**
@@ -30,17 +30,17 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_node_height;
-        size_t right_node_height;
+	size_t left_node_height;
+	size_t right_node_height;
 
-        if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-                return (0);
-        left_node_height = binary_tree_height(tree->left);
-        right_node_height = binary_tree_height(tree->right);
-        if (left_node_height >= right_node_height)
-                return (left_node_height + 1);
-        else
-                return (right_node_height + 1);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	left_node_height = binary_tree_height(tree->left);
+	right_node_height = binary_tree_height(tree->right);
+	if (left_node_height >= right_node_height)
+		return (left_node_height + 1);
+	else
+		return (right_node_height + 1);
 }
 
 /**
@@ -51,17 +51,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        int left_node_height = 0;
-        int right_node_height = 0;
+	int left_node_height = 0;
+	int right_node_height = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left != NULL)
-                left_node_height = binary_tree_height(tree->left) + 1;
-        if (tree->right != NULL)
-                right_node_height = binary_tree_height(tree->right) + 1;
-        return (left_node_height - right_node_height);
+	if (tree->left != NULL)
+		left_node_height = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		right_node_height = binary_tree_height(tree->right) + 1;
+	return (left_node_height - right_node_height);
 }
 
 /**
